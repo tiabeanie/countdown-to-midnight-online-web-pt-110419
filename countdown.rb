7a94e03a79c83e0d3aeb8dc4c8c6a54b2e20a125
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-08 18:31:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 counter = 10
 
-def countdown
+def countdown()
   while counter > 0 
-  puts "#{number} SECOND(S)!"
-  counter -= 1
+    puts "#{number} SECOND(S)!"
+    number = counter -= 1
   puts "HAPPY NEW YEAR!"
+end
 end
